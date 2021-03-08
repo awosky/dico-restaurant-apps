715c78f5b61cfe73bc1f3335c6e8f5f0b8022a02
Awosky <playergames998@gmail.com>
2021-03-08 10:06:12 +0700

    add websocket & push notif

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,13 +3,15 @@ const CONFIG = {
     BASE_IMAGE_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
     BASE_IMAGE_URL_FOODS: 'https://images.unsplash.com/photo-1512621776951-a57141f2eefd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
     BASE_IMAGE_URL_DRINK: 'https://images.unsplash.com/photo-1550247611-e651810312fe?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+    BASE_IMAGE_URL_WEBSOCKET: 'https://image.tmdb.org/t/p/w500/',
     FOOD_TYPE: 0,
     DRINK_TYPE:1,
-    // CACHE_NAME: 'Restaurant-v1',
-    CACHE_NAME: new Date().toISOString(), //dev only
+    CACHE_NAME: 'Restaurant-v1', // prod
+    // CACHE_NAME: new Date().toISOString(), //dev
     DATABASE_NAME: 'restaurant-database',
     DATABASE_VERSION: 1,
     OBJECT_STORE_NAME: 'restaurants',
+    WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
   };
   
   export default CONFIG;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,8 @@ import '../styles/main.css';
 import '../styles/responsive.css';
 import App from './views/app';
 import swRegister from './utils/sw-register';
+import WebSocketInitiator from './utils/websocket-initiator';
+import CONFIG from './globals/config';
 
 console.log('Hello Reviewers! :)');
 
@@ -19,4 +21,5 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', () => {
     app.renderPage();
     swRegister();
+    WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 });
